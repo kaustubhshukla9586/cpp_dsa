e183c7c72bd4cfe20c193f8c772d1caa7e7199b6
kaustubhshukla9586 <kaustubhshukla9586@gmail.com>
2026-03-25 22:58:23 +0530

    mueheheh

---
+++ C++/print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; // this uses std before all the functions like we don't have to write std::cout only cout will work.
+
+int main() {
+
+    // std is a lib and cout is a function with std::cout we call the function.
+    std::cout << "yo wwsup gng we diving deep in cpp\n"; // \n shifts to next line
+    std::cout << "yo wwsup gng we diving deep in chpp" << std::endl;
+    std::cout << "my nga";
+
+    return 0;
+}   
