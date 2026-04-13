6a24c6c5336e06bd4a8dfeb749bdb7931d1c0bba
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-13 10:24:09 +0530

    datatypes

---
+++ C++/datatypes_cpp.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    // int takes 4 bytes 
+    // unsigned int takes 4 bytes
+    // long int takes 4 bytes
+    // long long int takes 8 bytes 
+
+    // long long int x = 10;
+    // int y = sizeof(x);
+    // cout << y;
+
+    // for decimal numbers we use float or double. 
+    // float z = 5.6;
+    // double g = 7.8; 
+
+    // to take string input we use string and getline
+
+    string s;
+    cin >> s;
+    cout << s; // Only prints the first word of string
+     
+
+    return 0;
+}
