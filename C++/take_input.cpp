b43796203d0f1e5562e8f770d5e5980b5abd78ec
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-13 10:24:24 +0530

    new

--- C++/take_input.cpp
+++ C++/take_input.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <bits/stdc++.h> // This line imports all the c libraries at once 
 using namespace std;
 int main() {
 
-    int x,y;
+    int x,y; // 4 bytes for 1 variable
     cin >> x >> y;
     cout << "Number = " << x << " and y: " << y;
 
